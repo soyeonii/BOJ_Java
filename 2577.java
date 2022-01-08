614735f16a17bb8bbe36a5c5eda56b4abe68e137
Soyeon Lee <69310195+soyeonii@users.noreply.github.com>
2022-01-08 16:56:50 +0900

    Create 2577.java

---
+++ 2577.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int a = fr.nextInt();
+        int b = fr.nextInt();
+        int c = fr.nextInt();
+        int result = a * b * c;
+        String tmp = Integer.toString(result);
+
+        int[] count = new int[10];
+        for (int i = 0; i < tmp.length(); i++)
+            count[Integer.parseInt(tmp.charAt(i) + "")] += 1;
+            
+        for (int i = 0; i < 10; i++)
+            System.out.println(count[i]);
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
